f0b38553a32d44ae108c7ef66014e1e900181b46
Augusto Giavedoni <augusto.giavedoni@gmail.com>
2023-08-09 15:16:16 -0700

    refactor(database-models): change data type

--- db/sqlc/entry.sql.go
+++ db/sqlc/entry.sql.go
@@ -19,8 +19,8 @@ INSERT INTO entries (
 `
 
 type CreateEntryParams struct {
-	AccountID int64  `json:"account_id"`
-	Amount    string `json:"amount"`
+	AccountID int64 `json:"account_id"`
+	Amount    int64 `json:"amount"`
 }
 
 func (q *Queries) CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry, error) {
@@ -110,8 +110,8 @@ RETURNING id, account_id, amount, created_at
 `
 
 type UpdateEntryParams struct {
-	ID     int64  `json:"id"`
-	Amount string `json:"amount"`
+	ID     int64 `json:"id"`
+	Amount int64 `json:"amount"`
 }
 
 func (q *Queries) UpdateEntry(ctx context.Context, arg UpdateEntryParams) (Entry, error) {
